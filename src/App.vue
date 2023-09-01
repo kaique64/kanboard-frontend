e590e84732002b0051f5f376f350ee2066b33970
Costa Kaique <kaique.costa@br.bosch.com>
2023-09-01 12:35:27 -0300

    finish project configs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="text-h3">Hello</div>
-  {{ store.text }}
 </template>
 <script setup lang="ts">
-import { Test } from './store/teststore'
-
-const store = Test()
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,64 @@
 
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
-import { Quasar } from 'quasar';
+import { Buffer } from 'buffer';
+import { AppFullscreen, Loading, LocalStorage, Notify, Quasar } from 'quasar';
 
 import '@quasar/extras/material-icons/material-icons.css';
 
 import 'quasar/src/css/index.sass';
 import App from './App.vue';
 
-const pinia = createPinia();
-const app = createApp(App);
+const run = () => {
+  const global = window;
+  global.Buffer = Buffer;
+  window.process = {
+    env: {
+      NODE_ENV: import.meta.env.NODE_ENV
+    }
+  }
+  const modules = import.meta.globEager('/src/modules/*.ts');
+  Object.values(modules).forEach((module) => module.install?.(app));
 
-app.use(pinia)
-app.use(Quasar, {
-  plugins: {},
-});
+  const pinia = createPinia();
+  const app = createApp(App);
 
-app.mount('#app');
+  // In case of redeployed frontend the currently active users might have problems to dynamically load JS modules.
+  // For that problem we catch the resulting error and force a reloading of the page.
+  window.addEventListener('unhandledrejection', function(event) {
+    console.log(event);
+
+    if (event instanceof PromiseRejectionEvent && event.isTrusted) {
+      const messageToCheck = event.reason.toString();
+      console.log(messageToCheck);
+
+      const messagesToReload = [
+        'TypeError: Failed to fetch dynamically imported module',
+        'Error: Unable to preload CSS for',
+        'TypeError: error loading dynamically imported module'
+      ];
+
+      messagesToReload.forEach((messageToReload) => {
+        console.log(
+          'checking for reloadMessage starting with: ' + messageToReload
+        );
+        if (
+          messageToCheck.toLowerCase().startsWith(messageToReload.toLowerCase())
+        ) {
+          console.log('force reloading of the page...');
+          window.location.reload();
+        }
+      });
+    }
+  });
+
+  const config = {
+    plugins: { Notify, LocalStorage, AppFullscreen, Loading }
+  };
+
+  app.use(pinia)
+  app.use(Quasar, config);
+  app.mount('#app');
+}
+
+run();
