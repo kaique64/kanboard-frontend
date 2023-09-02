a437e71668c47b54d50dd5ed217a62be7a0aa8dc
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 16:49:57 -0300

    create board component

---
+++ src/modules/board/component/Board.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-card 
+        class="q-ma-md q-pa-md" 
+        style="min-width: 23rem;"
+    >
+        <span class="text-bold text-h6">{{ name }}</span>
+        <slot name="content"></slot>
+    </q-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface IProps {
+    name: string;
+}
+
+const props = defineProps<IProps>();
+const name = ref(props.name);
+
+</script>
