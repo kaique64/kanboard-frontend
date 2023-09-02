97501d6ffde8872f0281128c414cca058c2f3f32
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 17:04:40 -0300

    create task component

---
+++ src/modules/task/component/Task.vue
@@ -0,0 +1,20 @@
+<template>
+    <span class="text-bold">{{name}}</span>
+    <div v-if="description" class="column q-mt-sm">
+        <span class="text-bold">Description:</span>
+        {{description}}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface IProps {
+    name: string;
+    description?: string;
+}
+
+const props = defineProps<IProps>();
+const name = ref(props.name);
+const description = ref(props.description);
+</script>
