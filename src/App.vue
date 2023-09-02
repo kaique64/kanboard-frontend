55d27338f3b8f5bcdfb8a8686c24b77dd66c238f
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 18:27:49 -0300

    implement kanban component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <div v-for="board in boards">
-    {{ board.name }}
-    <Container 
+  <div class="row justify-center items-start full-width">
+    <Kanban 
       group-name="kanboard" 
-      @drag-start="handleDragStart(board.id, $event)" 
-      @drop="handleDrop(board.id, $event)"
-      :get-child-payload="getChildPayload"
-      :drop-placeholder="{ className: 'placeholder' }"
-    >            
-      <Draggable v-for="task in board.tasks" :key="task.id">
-        <div>
-            {{task.id}} -> {{task.name}}
-        </div>
-      </Draggable>
-    </Container>
+      :boards="boards"
+    />
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Container, Draggable } from "vue3-smooth-dnd";
+import Kanban from './modules/kanban/component/Kanban.vue'
 
-const boards = ref([
+const boards = [
 	{
 		"id": 2,
 		"created_at": "2023-09-02T05:13:15.000Z",
@@ -32,15 +21,15 @@ const boards = ref([
 				"created_at": "2023-09-02T17:28:07.000Z",
 				"updated_at": "2023-09-02T17:28:07.000Z",
 				"name": "Task 1 - Board 1",
-				"description": "",
+				"description": "Task 1 - Board 1 Description",
 				"boardId": 2
 			},
 			{
 				"id": 3,
 				"created_at": "2023-09-02T17:25:47.000Z",
 				"updated_at": "2023-09-02T17:28:33.000Z",
-				"name": "Task 1 - Board 1",
-				"description": null,
+				"name": "Task 2 - Board 1",
+				"description": "Task 2 - Board 1 Description",
 				"boardId": 2
 			}
 		]
@@ -56,7 +45,7 @@ const boards = ref([
 				"created_at": "2023-09-02T17:28:07.000Z",
 				"updated_at": "2023-09-02T17:28:07.000Z",
 				"name": "Task 1 - Board 2",
-				"description": "",
+				"description": 'Task 1 - Board 2 Description',
 				"boardId": 3
 			},
 			{
@@ -64,53 +53,12 @@ const boards = ref([
 				"created_at": "2023-09-02T17:25:47.000Z",
 				"updated_at": "2023-09-02T17:28:33.000Z",
 				"name": "Task 1 - Board 2",
-				"description": null,
+				"description": 'Task 2 - Board 2 Description',
 				"boardId": 3
 			}
     ]
 	}
-]);
-const draggingCard = ref({
-  boardId: -1,
-  index: -1,
-  data: {}
-});
-
-function handleDragStart(boardId: number, dragResult) {
-  const { payload, isSource } = dragResult;
-
-  if (isSource) {
-    draggingCard.value = {
-      boardId,
-      index: payload.index,
-      data: {
-        ...boards.value.find(i => i.id === boardId)?.tasks[payload.index]
-      }
-    }
-  }
-}
-
-function handleDrop(boardId: number, dropResult) {
-  const { removedIndex, addedIndex } = dropResult;
-
-  if (boardId === draggingCard.value.boardId && removedIndex === addedIndex) {
-    return;
-  }
-
-  if (removedIndex !== null) {
-    boards.value.find((i) => i.id === boardId)?.tasks.splice(removedIndex, 1);
-  }
-
-  if (addedIndex !== null) {
-    boards.value.find((i) => i.id === boardId)?.tasks.splice(addedIndex, 0, draggingCard.value.data);
-  }
-}
-
-function getChildPayload(index) {
-  return {
-    index
-  }
-}
+];
 </script>
 <style scoped>
 .placeholder {
