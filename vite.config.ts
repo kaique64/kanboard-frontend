126c82b2051f37fd940abc718e3bc1c074a30854
Costa Kaique <kaique.costa@br.bosch.com>
2023-09-01 23:05:19 -0300

    change configs to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { fileURLToPath } from 'url'
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
 export default defineConfig({
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
+  },
   plugins: [
     vue({
       template: { transformAssetUrls },
@@ -12,12 +23,20 @@ export default defineConfig({
       sassVariables: 'src/styles/quasar-variables.scss',
     }),
   ],
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: "util",
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
   server: {
     port: 3000
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  preview: {
+    host: "localhost",
+    port: 8080
+  }
 })
