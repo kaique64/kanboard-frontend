5113af08d47a5693e20814771632e93c53990600
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 17:54:28 -0300

    add mdi css

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import '@quasar/extras/material-icons/material-icons.css';
 
 import 'quasar/src/css/index.sass';
 import App from './App.vue';
+import '@quasar/extras/mdi-v6/mdi-v6.css'
 
 const run = () => {
   const global = window;
