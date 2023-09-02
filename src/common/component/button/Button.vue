a92afc893533020e221d25be98e0c08ef0d22814
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 18:24:19 -0300

    create button component

---
+++ src/common/component/button/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-btn 
+        :id="props.id"
+        :icon="props.icon"
+        :label="props.label"
+        :size="props.size"
+        :loading="props.loading"
+        :ripple="false"
+    >
+        <slot name="default"></slot>
+        <slot name="loading"></slot>
+    </q-btn>
+</template>
+
+<script setup lang="ts">
+type TTypeButton = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+interface IProps {
+    id: string;
+    label?: string;
+    icon?:string;
+    loading?: boolean;
+    type?: string;
+    size?: TTypeButton;
+}
+
+const props = defineProps<IProps>();
+</script>
