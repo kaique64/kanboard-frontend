9ee43545a772229f2dee1ae802426e14361f78e2
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 17:01:16 -0300

    create card component

---
+++ src/modules/card/component/Card.vue
@@ -0,0 +1,5 @@
+<template>
+    <q-card-section class="q-my-sm column rounded-borders" style="background-color: rgb(234, 234, 234);">
+        <slot name="content"></slot>
+    </q-card-section>
+</template>
