42a0e48d3316667c8dbaba00d870f8cec870b5b7
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 20:32:27 -0300

    create board store and remove board data from App file

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,10 @@
 <template>
   <div class="row justify-center items-start full-width">
-    <Kanban 
-      group-name="kanboard" 
-      :boards="boards"
-    />
+    <Kanban group-name="kanboard" />
   </div>
 </template>
 <script setup lang="ts">
-import Kanban from './modules/kanban/component/Kanban.vue'
-
-const boards = [
-	{
-		"id": 2,
-		"created_at": "2023-09-02T05:13:15.000Z",
-		"updated_at": "2023-09-02T05:33:52.000Z",
-		"name": "Board 1",
-		"tasks": [
-			{
-				"id": 4,
-				"created_at": "2023-09-02T17:28:07.000Z",
-				"updated_at": "2023-09-02T17:28:07.000Z",
-				"name": "Task 1 - Board 1",
-				"description": "Task 1 - Board 1 Description",
-				"boardId": 2
-			},
-			{
-				"id": 3,
-				"created_at": "2023-09-02T17:25:47.000Z",
-				"updated_at": "2023-09-02T17:28:33.000Z",
-				"name": "Task 2 - Board 1",
-				"description": "Task 2 - Board 1 Description",
-				"boardId": 2
-			}
-		]
-	},
-	{
-		"id": 3,
-		"created_at": "2023-09-02T05:40:24.000Z",
-		"updated_at": "2023-09-02T05:40:24.000Z",
-		"name": "Board 2",
-		"tasks": [
-    {
-				"id": 5,
-				"created_at": "2023-09-02T17:28:07.000Z",
-				"updated_at": "2023-09-02T17:28:07.000Z",
-				"name": "Task 1 - Board 2",
-				"description": 'Task 1 - Board 2 Description',
-				"boardId": 3
-			},
-			{
-				"id": 6,
-				"created_at": "2023-09-02T17:25:47.000Z",
-				"updated_at": "2023-09-02T17:28:33.000Z",
-				"name": "Task 1 - Board 2",
-				"description": 'Task 2 - Board 2 Description',
-				"boardId": 3
-			}
-    ]
-	}
-];
+import Kanban from './modules/kanban/component/Kanban.vue';
 </script>
 <style scoped>
 .placeholder {
---
+++ src/modules/board/store/BoardStore.ts
@@ -0,0 +1,30 @@
+import { Ref, ref } from "vue";
+import { defineStore } from "pinia";
+import { BoardDTO } from "../dtos/BoardDTO";
+import { RestService } from "../../../common/service/integration/RestService";
+
+const BoardStore = defineStore('BoardStore', () => {
+    const restService = new RestService();
+    const boards: Ref<BoardDTO[]> = ref([]);
+
+    const setBoards = (boardsDTO: BoardDTO[]) => boards.value = boardsDTO;
+    const getBoards = () => boards.value;
+
+    async function fetchBoard() {
+        const boardsData: BoardDTO[] | null = await restService.executeGet<BoardDTO[]>({
+            url: `/board`
+        });
+        
+        if (boardsData) {
+            setBoards(boardsData);
+        }
+    }
+
+    return {
+        fetchBoard,
+        setBoards,
+        getBoards,
+    }
+});
+
+export default BoardStore;
