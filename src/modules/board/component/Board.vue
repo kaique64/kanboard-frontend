2af3923604faf0a8de0a6cc028ea8632c0647251
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 18:27:30 -0300

    add button to insert new tasks

--- src/modules/board/component/Board.vue
+++ src/modules/board/component/Board.vue
@@ -3,13 +3,22 @@
         class="q-ma-md q-pa-md" 
         style="min-width: 23rem;"
     >
-        <span class="text-bold text-h6">{{ name }}</span>
+        <div class="row justify-between">
+            <span class="text-bold text-h6">{{ name }}</span>
+            <div>
+                <Button id="action-button" flat icon="mdi-dots-horizontal" />
+                <q-menu anchor="bottom start">
+                    <Button id="add-task-button" no-caps icon="mdi-plus" label="Add new task"></Button>
+                </q-menu>
+            </div>
+        </div>
         <slot name="content"></slot>
     </q-card>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import Button from '../../../common/component/button/Button.vue';
 
 interface IProps {
     name: string;
@@ -17,5 +26,4 @@ interface IProps {
 
 const props = defineProps<IProps>();
 const name = ref(props.name);
-
 </script>
