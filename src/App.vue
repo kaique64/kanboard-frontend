b2ea887ca772bded368d40802cd738cd261da559
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 15:04:50 -0300

    create mocked items and add styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="text-h3">Hello</div>
+  <div class="drop-zone">
+
+  </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    id: 0,
+    title: 'Item A',
+    list: 1,
+  },
+  {
+    id: 1,
+    title: 'Item B',
+    list: 1,
+  },
+  {
+    id: 2,
+    title: 'Item C',
+    list: 2,
+  },
+]);
 </script>
+<style scoped>
+.drop-zone {
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+
+.drag-el {
+  background-color: #fff;
+  margin-bottom: 10px;
+  padding: 5px;
+}
+</style>
+
