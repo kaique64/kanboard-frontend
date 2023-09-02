68f76bfa1e099008917974966f62b5531ae7129b
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 20:24:29 -0300

    create environments variables type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_ROOT_DIRECTORY: string
+    readonly VITE_APP_REDIRECT_URI: string
+    readonly VITE_API_URI: string   
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
