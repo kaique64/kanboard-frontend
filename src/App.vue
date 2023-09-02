e227627c240783a6d843e023451775e350570c0e
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-02 16:19:57 -0300

    create a preview kanban with mocked data

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,121 @@
 <template>
-  <div class="drop-zone">
-
+  <div v-for="board in boards">
+    {{ board.name }}
+    <Container 
+      group-name="kanboard" 
+      @drag-start="handleDragStart(board.id, $event)" 
+      @drop="handleDrop(board.id, $event)"
+      :get-child-payload="getChildPayload"
+      :drop-placeholder="{ className: 'placeholder' }"
+    >            
+      <Draggable v-for="task in board.tasks" :key="task.id">
+        <div>
+            {{task.id}} -> {{task.name}}
+        </div>
+      </Draggable>
+    </Container>
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
+import { Container, Draggable } from "vue3-smooth-dnd";
 
-const items = ref([
-  {
-    id: 0,
-    title: 'Item A',
-    list: 1,
-  },
-  {
-    id: 1,
-    title: 'Item B',
-    list: 1,
-  },
-  {
-    id: 2,
-    title: 'Item C',
-    list: 2,
-  },
+const boards = ref([
+	{
+		"id": 2,
+		"created_at": "2023-09-02T05:13:15.000Z",
+		"updated_at": "2023-09-02T05:33:52.000Z",
+		"name": "Board 1",
+		"tasks": [
+			{
+				"id": 4,
+				"created_at": "2023-09-02T17:28:07.000Z",
+				"updated_at": "2023-09-02T17:28:07.000Z",
+				"name": "Task 1 - Board 1",
+				"description": "",
+				"boardId": 2
+			},
+			{
+				"id": 3,
+				"created_at": "2023-09-02T17:25:47.000Z",
+				"updated_at": "2023-09-02T17:28:33.000Z",
+				"name": "Task 1 - Board 1",
+				"description": null,
+				"boardId": 2
+			}
+		]
+	},
+	{
+		"id": 3,
+		"created_at": "2023-09-02T05:40:24.000Z",
+		"updated_at": "2023-09-02T05:40:24.000Z",
+		"name": "Board 2",
+		"tasks": [
+    {
+				"id": 5,
+				"created_at": "2023-09-02T17:28:07.000Z",
+				"updated_at": "2023-09-02T17:28:07.000Z",
+				"name": "Task 1 - Board 2",
+				"description": "",
+				"boardId": 3
+			},
+			{
+				"id": 6,
+				"created_at": "2023-09-02T17:25:47.000Z",
+				"updated_at": "2023-09-02T17:28:33.000Z",
+				"name": "Task 1 - Board 2",
+				"description": null,
+				"boardId": 3
+			}
+    ]
+	}
 ]);
-</script>
-<style scoped>
-.drop-zone {
-  background-color: #eee;
-  margin-bottom: 10px;
-  padding: 10px;
+const draggingCard = ref({
+  boardId: -1,
+  index: -1,
+  data: {}
+});
+
+function handleDragStart(boardId: number, dragResult) {
+  const { payload, isSource } = dragResult;
+
+  if (isSource) {
+    draggingCard.value = {
+      boardId,
+      index: payload.index,
+      data: {
+        ...boards.value.find(i => i.id === boardId)?.tasks[payload.index]
+      }
+    }
+  }
 }
 
-.drag-el {
-  background-color: #fff;
-  margin-bottom: 10px;
-  padding: 5px;
+function handleDrop(boardId: number, dropResult) {
+  const { removedIndex, addedIndex } = dropResult;
+
+  if (boardId === draggingCard.value.boardId && removedIndex === addedIndex) {
+    return;
+  }
+
+  if (removedIndex !== null) {
+    boards.value.find((i) => i.id === boardId)?.tasks.splice(removedIndex, 1);
+  }
+
+  if (addedIndex !== null) {
+    boards.value.find((i) => i.id === boardId)?.tasks.splice(addedIndex, 0, draggingCard.value.data);
+  }
+}
+
+function getChildPayload(index) {
+  return {
+    index
+  }
+}
+</script>
+<style scoped>
+.placeholder {
+  background: rgba(0, 0, 0);
+  border-radius: 0.4rem;
 }
 </style>
 
