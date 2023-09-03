35952d94fcb264c1a3d3c5d5cf91b2fc80d4e025
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-03 13:34:20 -0300

    create dialog component to add new board

---
+++ src/modules/board/component/AddBoardDialog.vue
@@ -0,0 +1,54 @@
+<template>
+    <q-dialog v-model="props.modelValue" persistent>
+        <DialogTemplate height="50vh" width="40vw" :title="title" close-btn :click-close="() => $emit('close')">
+            <template #content>
+                <BoardForm 
+                    @cancel="$emit('cancel')" 
+                    @submit="handleAddTask" 
+                    submit-button-label="Add"
+                />
+            </template>
+        </DialogTemplate>
+    </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+import DialogTemplate from '../../../common/component/dialog/DialogTemplate.vue'
+import { BoardFormDTO } from '../types/dtos/BoardFormDTO';
+import BoardForm from './/BoardForm.vue';
+
+interface IProps {
+    modelValue: any;
+    title: string;
+    boardId: number;
+}
+
+const emit = defineEmits(['close', 'cancel', 'onAdd']);
+const props = defineProps<IProps>();
+const boardService = BoardService();
+const $q = useQuasar();
+
+async function handleAddTask(formValues: BoardFormDTO, resetForm: () => void) {
+    await addTask(formValues);
+    emit('close');
+    resetForm();
+}
+
+async function addTask(formValues: BoardFormDTO) {
+  const board = {
+    name: formValues.name,
+  }
+
+  const boardCreated = await boardService.createBoard(board);
+
+  if (boardCreated) {
+    $q.notify({
+        message: 'Task created successfully!',
+        type: 'positive',
+        color: 'green-5',
+        position: 'top-right'
+    })
+  }
+}
+</script>
