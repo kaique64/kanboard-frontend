df88fc9e26c565724eb5764127c04a6071e68c22
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-03 13:43:02 -0300

    create method to create, update and delete a board

--- src/modules/board/store/BoardStore.ts
+++ src/modules/board/store/BoardStore.ts
@@ -2,6 +2,7 @@ import { Ref, ref } from "vue";
 import { defineStore } from "pinia";
 import { BoardDTO } from "../types/dtos/BoardDTO";
 import { RestService } from "../../../common/service/integration/RestService";
+import { BoardFormDTO } from "../types/dtos/BoardFormDTO";
 
 const BoardStore = defineStore('BoardStore', () => {
     const restService = new RestService();
@@ -13,6 +14,32 @@ const BoardStore = defineStore('BoardStore', () => {
 
     const setLoading = (loadingValue: boolean) => loading.value = loadingValue;
     const getLoading = () => loading.value;
+
+    async function createBoard(boardDTO: BoardFormDTO): Promise<BoardDTO | null> {
+        const board: BoardDTO | null = await restService.executePost<BoardDTO, BoardFormDTO>({
+            url: '/board',
+            payload: boardDTO,
+        });
+
+        return board;
+    }
+
+    async function updateBoard(id: number, boardDTO: BoardFormDTO): Promise<BoardDTO | null> {
+        const board: BoardDTO | null = await restService.executePut<BoardDTO, BoardFormDTO>({
+            url: `/board/${id}`,
+            payload: boardDTO,
+        });
+
+        return board;
+    }
+
+    async function deleteBoard(id: number): Promise<BoardDTO | null> {
+        const board: BoardDTO | null = await restService.executeDelete<BoardDTO>({
+            url: `/board/${id}`
+        });
+
+        return board;
+    }
 
     async function fetchBoard() {
         const boardsData: BoardDTO[] | null = await restService.executeGet<BoardDTO[]>({
@@ -26,6 +53,9 @@ const BoardStore = defineStore('BoardStore', () => {
 
     return {
         fetchBoard,
+        createBoard,
+        updateBoard,
+        deleteBoard,
         setBoards,
         getBoards,
         setLoading,
