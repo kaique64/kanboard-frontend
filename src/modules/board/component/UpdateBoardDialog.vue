7cced1df76063c3c33a0f12b307fbff5fcff246f
kaique64 <kaiquehenrique.2005@outlook.com>
2023-09-03 14:17:12 -0300

    create dialog to update a board

---
+++ src/modules/board/component/UpdateBoardDialog.vue
@@ -0,0 +1,61 @@
+<template>
+    <q-dialog v-model="props.modelValue" persistent>
+        <DialogTemplate height="50vh" width="40vw" :title="title" close-btn :click-close="() => $emit('close')">
+            <template #content>
+                <BoardForm 
+                    :board-form="boardForm"
+                    @cancel="$emit('cancel')" 
+                    @submit="(form, resetForm) => {
+                        handleUpdateTask(form, form.id, resetForm);
+                    }" 
+                    submit-button-label="Update"
+                />
+            </template>
+        </DialogTemplate>
+    </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useQuasar } from 'quasar';
+import DialogTemplate from '../../../common/component/dialog/DialogTemplate.vue'
+import { BoardFormDTO } from '../types/dtos/BoardFormDTO';
+import BoardForm from './/BoardForm.vue';
+import { BoardService } from '../service/BoardService';
+import { BoardDTO } from '../types/dtos/BoardDTO';
+
+interface IProps {
+    modelValue: any;
+    title: string;
+    boardForm: BoardDTO;
+}
+
+const emit = defineEmits(['close', 'cancel', 'onAdd']);
+const props = defineProps<IProps>();
+const boardForm = computed(() => props.boardForm);
+const boardService = BoardService();
+const $q = useQuasar();
+
+async function handleUpdateTask(formValues: BoardFormDTO, baordId: number, resetForm: () => void) {
+    await updateTask(formValues, baordId);
+    emit('close');
+    resetForm();
+}
+
+async function updateTask(formValues: BoardFormDTO, baordId: number) {
+  const board = {
+    name: formValues.name,
+  }
+
+  const boardCreated = await boardService.updateBoard(baordId, board);
+
+  if (boardCreated) {
+    $q.notify({
+        message: 'Board updated successfully!',
+        type: 'positive',
+        color: 'green-5',
+        position: 'top-right'
+    })
+  }
+}
+</script>
